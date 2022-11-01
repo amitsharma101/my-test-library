005c96984f90580ec144f6efd61d7814fa7508a2
Amit Sharma <amitsharma101@icloud.com>
2022-11-01 15:34:42 +0530

    added login activity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.mylibrary.FirstClass
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            FirstClass().showToast(this, "Some Toast Here")
+        }
     }
 }
